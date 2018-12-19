0230c01df4cb86bb1b9883a05c81c72e0f1cd9ec
ZDragon17 <2737497930@qq.com>
2018-12-19 21:48:33 +0800

    更改获取问题分页云函数

--- cloudfunctions/getQuestions/index.js
+++ cloudfunctions/getQuestions/index.js
@@ -7,13 +7,13 @@ const questions = db.collection("questions")
 // 云函数入口函数
 exports.main = async (event, context) => {
   const res = await questions.field({
-    _id:false,
+    _id:true,
     correct_answer:true,
     answer_a:true,
     answer_b:true,
     answer_c:true,
     answer_d:true,
     questions:true
-  }).get();
+  }).limit(event.rows).skip(event.page).get();
   return res;
 }
