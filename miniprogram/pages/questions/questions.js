b390f8ac4bc5a99c7dca898b94920e672d7d0290
tianxuning <xtian513@gmail.com>
2018-12-19 18:11:53 +0800

    判断题目正确性

--- miniprogram/pages/questions/questions.js
+++ miniprogram/pages/questions/questions.js
@@ -5,19 +5,52 @@ Page({
    * 页面的初始数据
    */
   data: {
-    windowHeight: null,
+    windowHeight: null, //屏幕高度
+    
+    //临时
+    currentIndex: 0, //当前题目下标
+    questionList: null, //问题list
+    currentQuestion: null, //当前问题
+    // currectSelections: [], //当前选项
   },
 
   getQuestions(){
+    let that = this;
+    let { currentIndex} = this.data;
     wx.cloud.callFunction({
       // 云函数名称
       name: 'getQuestions',
       // 传给云函数的参数
-    })
-      .then(res => {
+    }).then(res => {
         console.log(res.result) // 3
+        that.setData({
+          questionList: res.result.data,
+          currentQuestion: res.result.data[currentIndex]
+        })
       })
       .catch(console.error)
+  },
+
+  awsClick(e){
+    let { aws } = e.currentTarget.dataset; 
+    console.log(aws)
+    this.checkAnswer(aws)
+  },
+
+  checkAnswer(aws){
+    let { currentQuestion } = this.data;
+    wx.cloud.callFunction({
+      // 云函数名称
+      name: 'checkAnswer',
+      data: {
+        questionId: currentQuestion._id,
+        answer: aws
+      }
+      // 传给云函数的参数
+    }).then(res => {
+      console.log(res.result) // 3
+     
+    }).catch(console.error)
   },
 
   /**
@@ -34,6 +67,7 @@ Page({
         })
       },
     })
+
   },
 
   /**
